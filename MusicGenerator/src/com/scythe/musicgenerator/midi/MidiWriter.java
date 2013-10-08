3382b28ac0f73be95b4c5b75f49a5346db2448c7
Alexis <alexis.troncy@awabot.com>
2013-10-08 13:52:28 +0200

    MidiWriter empty pattern added

---
+++ MusicGenerator/src/com/scythe/musicgenerator/midi/MidiWriter.java
@@ -0,0 +1,13 @@
+package com.scythe.musicgenerator.midi;
+
+import java.util.ArrayList;
+
+import com.scythe.musicgenerator.core.Bar;
+
+public class MidiWriter
+{
+	public static void writeFile(ArrayList<Bar> bars, String fileName)
+	{
+		
+	}
+}
